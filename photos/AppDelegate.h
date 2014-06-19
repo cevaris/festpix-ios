53057e639de49a72794094133ebc9f9371a1396e
Cevaris <cevaris@gmail.com>
2014-06-18 20:16:58 -0600

    first commit

---
+++ photos/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  photos
+//
+//  Created by Cevaris on 6/18/14.
+//  Copyright (c) 2014 cevaris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ photos/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  photos
+//
+//  Created by Cevaris on 6/18/14.
+//  Copyright (c) 2014 cevaris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
