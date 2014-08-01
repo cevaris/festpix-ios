f598b22739ffb9bbdeb9a74773abecb22dfe70e3
Adam Cardenas <cevaris@gmail.com>
2014-07-31 20:49:59 -0600

    worked on table

---
+++ photos/PhotoSessionListViewController.h
@@ -0,0 +1,13 @@
+//
+//  PhotoSessionListViewController.h
+//  photos
+//
+//  Created by Cevaris on 7/31/14.
+//  Copyright (c) 2014 cevaris. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface PhotoSessionListViewController : ViewController
+
+@end
---
+++ photos/PhotoSessionListViewController.m
@@ -0,0 +1,13 @@
+//
+//  PhotoSessionListViewController.m
+//  photos
+//
+//  Created by Cevaris on 7/31/14.
+//  Copyright (c) 2014 cevaris. All rights reserved.
+//
+
+#import "PhotoSessionListViewController.h"
+
+@implementation PhotoSessionListViewController
+
+@end
